62527659e8632a16ef6ee7f0f31d284bdf48031e
samvelie <sam.velie@gmail.com>
2017-03-11 14:59:47 -0600

    view updating with salary calculation

--- server/public/scripts/factories/data.factory.js
+++ server/public/scripts/factories/data.factory.js
@@ -1,7 +1,10 @@
 myApp.factory('DataFactory', ['$http', function($http) {
   console.log("data factory running");
 
-  var employeeData = {list:[]};
+  var employeeData = {
+    list:[],
+    yearSalary: 0
+  };
 
   function getEmployeeData(){
     $http({
@@ -11,11 +14,20 @@ myApp.factory('DataFactory', ['$http', function($http) {
       console.log('response', response);
       console.log('response.data', response.data);
       employeeData.list = response.data;
-    })
+      employeeData.yearSalary = yearlyTotal(employeeData.list);
+    });
+  }
+
+  function yearlyTotal(employeeArray){
+    var salaryTotal = 0;
+    for (var i = 0; i < employeeArray.length; i++) {
+      salaryTotal += parseFloat(employeeArray[i].salary);
+    }
+    return salaryTotal;
   }
 
   return {
     employees: employeeData,
     getEmployees: getEmployeeData
-  }
+  };
 }]);
