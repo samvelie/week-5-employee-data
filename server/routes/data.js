aca06d0d1f82bf33d1d47d079ee640452f338c4c
samvelie <sam.velie@gmail.com>
2017-03-11 18:19:42 -0600

    new employee post working

--- server/routes/data.js
+++ server/routes/data.js
@@ -30,11 +30,31 @@ router.get('/employees', function(req, res) {
       }); // end client.query
     }
   });
-});//end router.get
+});//end employees router.get
 
 router.post('/employees', function(req,res) {
   console.log('hit my post employees route');
-  console.log(req.body);
-});
+  var newEmployee = req.body;
+  console.log(newEmployee + ' being added');
+  pool.connect(function(err, client, done) {
+    if(err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+        client.query('INSERT INTO employees(first_name, last_name, employee_number, job_title, salary) VALUES($1, $2, $3, $4, $5);',
+          [newEmployee.firstName, newEmployee.lastName, newEmployee.employeeID, newEmployee.jobTitle, newEmployee.salary],
+          function(err, result) {
+            done();
+            if(err) {
+              console.log(err);
+              res.sendStatus(500);
+            } else {
+                res.sendStatus(201);
+              }
+          }
+        );
+      }
+  });//end pool.connect
+});//end employees router.post
 
 module.exports = router;
